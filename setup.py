adc65a6dbfab6a3bffb2c092efccc7e23ca21a70
Dusty Phillips <dusty@archlinux.ca>
2010-06-24 10:24:32 -0600

    Increment release.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
         name="django-pytest",
-        version="0.1.0",
+        version="0.1.1",
         author="Dusty Phillips",
         author_email="dusty@archlinux.ca",
         packages=['django_pytest'],
