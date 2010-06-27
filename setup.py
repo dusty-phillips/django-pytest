07d545dab729d7f746dc515feeb48ad55bc374f2
Dusty Phillips <dusty@archlinux.ca>
2010-06-27 14:02:54 -0600

    STILL packaging errors. I hate this part.

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
         name="django-pytest",
-        version="0.1.2",
+        version="0.1.3",
         author="Dusty Phillips",
         author_email="dusty@archlinux.ca",
-        packages=['django_pytest', 'django_pytest.management.commands'],
+        packages=['django_pytest', 'django_pytest.management.commands', 'django_pytest.management'],
         url="http://github.com/buchuki/django-pytest",
         license="LICENSE.txt",
         description="django test runner to use py.test tests",
