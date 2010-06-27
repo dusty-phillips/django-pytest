9ec987f1ac2799131a0cd5f09eb29117740aa4b2
Dusty Phillips <dusty@archlinux.ca>
2010-06-27 13:56:42 -0600

    Packaging error wasn't adding all packages.

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from distutils.core import setup
 
 setup(
         name="django-pytest",
-        version="0.1.1",
+        version="0.1.2",
         author="Dusty Phillips",
         author_email="dusty@archlinux.ca",
-        packages=['django_pytest'],
+        packages=['django_pytest', 'django_pytest.management.commands'],
         url="http://github.com/buchuki/django-pytest",
         license="LICENSE.txt",
         description="django test runner to use py.test tests",
