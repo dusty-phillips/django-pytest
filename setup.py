48d20024c2cfd04a3359b5ed314fa07a22827a5f
Dusty Phillips <dusty@archlinux.ca>
2010-08-22 14:13:53 -0600

    Make setup.py more automatic.

--- setup.py
+++ setup.py
@@ -1,13 +1,15 @@
-from distutils.core import setup
+from setuptools import setup
+from setuptools import find_packages
 
 setup(
         name="django-pytest",
-        version="0.1.3",
+        version="0.1.4",
         author="Dusty Phillips",
         author_email="dusty@archlinux.ca",
-        packages=['django_pytest', 'django_pytest.management.commands', 'django_pytest.management'],
+        packages=find_packages(),
         url="http://github.com/buchuki/django-pytest",
         license="LICENSE.txt",
         description="django test runner to use py.test tests",
         long_description=open('README.txt').read(),
+        zip_safe=False,
 )
