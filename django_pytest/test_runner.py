7073d7be0fdc19014424ad96e2abdf075a239a32
Dusty Phillips <dusty@archlinux.ca>
2011-01-11 15:01:49 -0700

    Issue #2 update to depend on pytest 2.0 instead of pylib.

--- django_pytest/test_runner.py
+++ django_pytest/test_runner.py
@@ -4,5 +4,5 @@ def run_tests(test_labels, verbosity=1, interactive=True, extra_tests=[]):
     sys.argv[1:] = sys.argv[2:]
 
     sys.exit(
-       load_entry_point('py>=1.0.0', 'console_scripts', 'py.test')()
+       load_entry_point('pytest>=2.0', 'console_scripts', 'py.test')()
     )
