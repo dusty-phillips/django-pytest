342fb49cfab82ba77e44502caebc404269d52abe
Dusty Phillips <dusty@archlinux.ca>
2011-01-11 16:25:09 -0700

    That's what I get for reading, thinking, forgetting, waiting weeks, then applying without reading, or thinking.

--- django_pytest/test_runner.py
+++ django_pytest/test_runner.py
@@ -3,6 +3,9 @@ def run_tests(test_labels, verbosity=1, interactive=True, extra_tests=[]):
     from pkg_resources import load_entry_point
     sys.argv[1:] = sys.argv[2:]
 
-    sys.exit(
-       load_entry_point('pytest>=2.0', 'console_scripts', 'py.test')()
-    )
+    try:
+        entry_point = load_entry_point('py>=1.0.0', 'console_scripts', 'py.test')
+    except ImportError:
+        entry_point = load_entry_point('pytest>=2.0', 'console_scripts', 'py.test')
+
+    sys.exit(entry_point())
