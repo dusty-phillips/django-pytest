1ccbed638d4e4da1ab8472211d6117cfa6ee114c
Dusty Phillips <dusty@archlinux.ca>
2011-10-25 17:46:43 +0200

    Point release 0.1.5

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
         name="django-pytest",
-        version="0.1.4",
+        version="0.1.5",
         author="Dusty Phillips",
         author_email="dusty@archlinux.ca",
         packages=find_packages(),
