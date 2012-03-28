7d5c26104d05757badd22691530ea73ebf6c98ab
Dusty Phillips <dusty@buchuki.com>
2012-03-28 10:56:35 -0600

    Prepare 0.2.0 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
         name="django-pytest",
-        version="0.1.5",
+        version="0.2.0",
         author="Dusty Phillips",
         author_email="dusty@archlinux.ca",
         packages=find_packages(),
